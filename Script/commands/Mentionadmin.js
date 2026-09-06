6dc3ea5ae1a358918f1e6cf5c19ef18b31101967
myoun12 <mdmyoun256@gmail.com>
2026-09-06 18:43:06 +0600

    Update admin IDs in Mentionadmin.js
    Removed two admin IDs and added a new one to the list.

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -11,9 +11,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event }) {
   const adminIDs = [
-    "100089047474463",
-    "100001039692046",
-    "100044713412032"
+    "100021922069388",
+    "",
+    ""
   ].map(String);
 
   if (adminIDs.includes(String(event.senderID))) return;
